6eb232028eb721716a890f357e49c76c94cc61a6
Sohaib Furqan <sohaibfurqan@gmail.com>
2021-10-13 17:11:52 +0100

    Completed video footer layout

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import Ticker from 'react-ticker';
 
 const VideoFooter = () => {
   return (
@@ -7,7 +8,16 @@ const VideoFooter = () => {
       <div className='videoFooter_text'>
         <h3>@sohaibfurqan</h3>
         <p>This is a description</p>
-        <MusicNoteIcon />
+        <div className='videoFooter_ticker'>
+          <MusicNoteIcon className='videoFooter_icon' />
+          <Ticker mode='smooth'>
+            {({ index }) => (
+              <>
+                <p>Yooooo people! I love you!</p>
+              </>
+            )}
+          </Ticker>
+        </div>
       </div>
       <img
         src='https://static.thenounproject.com/png/934821-200.png'
